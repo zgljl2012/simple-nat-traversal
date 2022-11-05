373a8b0da2828b4f909e899e828d9e5b3b7c5382
zgljl2012 <2693491512@qq.com>
2022-11-05 21:59:28 +0800

    client reconnect

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,6 @@
 //! Simple websocket client.
+use std::{time::Duration, thread};
+
 use nat::NatClient;
 
 pub struct ClientConfig {
@@ -8,6 +10,14 @@ pub struct ClientConfig {
 pub async fn start_client(config: &ClientConfig) -> Result<(), Box<dyn std::error::Error>> {
     log::info!("starting NAT client: {}", config.server_url);
     let mut client = NatClient::new(config.server_url.as_str()).await.unwrap();
-    client.run_forever().await?;
-    Ok(())
+	loop {
+		match client.run_forever().await {
+			Ok(_) => {},
+			Err(err) => {
+				log::error!("{}", err);
+				log::info!("Auto reconnect...");
+				thread::sleep(Duration::from_millis(3000));
+			}
+		};
+	}
 }
